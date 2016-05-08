d970716f437c0135bd18e8c1bec1ae0bc6839221
Jannis Hell <jh@mixedzone.io>
2016-05-08 15:47:15 +0200

    Add own fork instead of original source.

--- js/source/scripts.js
+++ js/source/scripts.js
@@ -10,47 +10,49 @@
 // wrapping it with an "anonymous closure". See:
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-(function ($, Drupal, window, document) {
+(function($, Drupal, window, document) {
 
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
   Drupal.behaviors.eyesbound = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       //var $cycle = $('.cycle').hide();
       //var $cycleImg = $cycle.find('img').hide();
 
-      $(document).ready(function () {
+      $(document).ready(function() {
         // Execute code once the DOM is ready.
-          $('.cycle').fadeSlideShow({
-              width: false,
-              height: false,
-              PlayPauseElement: false,
-              NextElementText: '»',
-              PrevElementText: '«',
-              ListElement: false
-          });
+        $('.cycle').fadeSlideShow({
+          width: false,
+          height: false,
+          PlayPauseElement: false,
+          NextElementText: '»',
+          PrevElementText: '«',
+          ListElement: false,
+          beforeSlide: function() {console.log("go")},
+          afterSlide: function() {console.log("done")}
+        });
         //$cycle = $('.cycle').hide();
         //$cycleImg = $cycle.find('img').hide();
       });
 
-      $(window).load(function () {
+      $(window).load(function() {
         // Execute code once the window is fully loaded.
         /*$cycleImg.each(function(){
-          $(this).parent().css('background-image', 'url('+this.currentSrc+')');
-        });
-        $cycle.show();*/
+         $(this).parent().css('background-image', 'url('+this.currentSrc+')');
+         });
+         $cycle.show();*/
       });
 
-      $(window).resize(function () {
+      $(window).resize(function() {
         // Execute code when the window is resized.
         /*$cycleImg.each(function(){
-          $(this).parent().css('background-image', 'url('+this.currentSrc+')');
-        });*/
+         $(this).parent().css('background-image', 'url('+this.currentSrc+')');
+         });*/
       });
 
-      $(window).scroll(function () {
+      $(window).scroll(function() {
         // Execute code when the window scrolls.
       });
     }
   };
 
-} (jQuery, Drupal, this, this.document));
+}(jQuery, Drupal, this, this.document));
