ff91c1bc4738c6354e1ded958c6b2ef5736b6e09
Primajin <github@eyesbound.com>
2020-11-25 19:56:03 +0100

    add homepage pictures to index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>;
-}
+const Home = ({ data }) => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Get only homepage=true pictures:</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+};
+
+export default Home;
+
+export const pageQuery = graphql`
+  query Home {
+    allPrismicPicture(filter: { data: { homepage: { eq: true } } }) {
+      edges {
+        node {
+          uid
+        }
+      }
+    }
+  }
+`;
