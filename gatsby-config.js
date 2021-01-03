e21fe19f768ca02f538a36953399fba69974ae36
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-03 17:24:38 +0100

    add eslint with prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,28 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
   ],
 };
