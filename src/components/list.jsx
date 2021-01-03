159c18e9d2298c54480d8a11a63719932bd29c14
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-03 15:23:14 +0100

    auto format from CRLF to LF

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const List = ({ title, data, path }) => {
   return (
     <>
-      <h1>{title}</h1>
+      <h1>
+        {title} ({data.length})
+      </h1>
       <ul>
         {data.map(({ node: { uid, data: { title } } }) => (
           <li key={uid}>
