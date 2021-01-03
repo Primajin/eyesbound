5a3e45843ba39f3886e73a7b14da654cca5890ba
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-03 19:59:22 +0100

    add slideshow

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
 import Picture from '../components/picture';
 
 const Home = ({
@@ -7,6 +10,21 @@ const Home = ({
     allPrismicPicture: { edges },
   },
 }) => {
+  const {
+    node: { id, uid },
+  } = edges[0];
+
+  const properties = {
+    indicators: i => (
+      <li className="indicator">
+        <figure>
+          <img src={`https://placebear.com/${i + 300}/${i + 200}`} alt={`Bear ${i + 1}`} width="25" height="25" />
+          <figcaption>{i + 1}</figcaption>
+        </figure>
+      </li>
+    ),
+  };
+
   return (
     <>
       <h1>Home</h1>
@@ -17,6 +35,23 @@ const Home = ({
         </a>
       ))}
       <pre>{JSON.stringify(edges, null, 2)}</pre>
+
+      <div className="slide-container">
+        <Fade {...properties}>
+          <figure key={id}>
+            <img src="https://placebear.com/300/200" alt="Bear 1" width="300" height="200" />
+            <figcaption>{uid} 1</figcaption>
+          </figure>
+          <figure key={id}>
+            <img src="https://placebear.com/301/201" alt="Bear 2" width="300" height="200" />
+            <figcaption>{uid} 2</figcaption>
+          </figure>
+          <figure key={id}>
+            <img src="https://placebear.com/302/202" alt="Bear 3" width="300" height="200" />
+            <figcaption>{uid} 3</figcaption>
+          </figure>
+        </Fade>
+      </div>
     </>
   );
 };
