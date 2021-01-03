2a1d25b308dfa9aa9986f0c88c377e857594b62b
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-03 15:54:59 +0100

    add back link for dev

---
+++ src/components/404-link.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundLink = () => {
+  return <a href="/404">Back to 404 page</a>;
+};
+
+export default NotFoundLink;
--- src/templates/series-all.jsx
+++ src/templates/series-all.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import List from '../components/list';
+import NotFoundLink from '../components/404-link';
 
 const Series = ({ data: { allPrismicSeries } }) => {
   const { edges } = allPrismicSeries;
   return (
     <>
+      <NotFoundLink />
       <List title="Series" data={edges} path="series" />
       <h2>Raw Data:</h2>
       <pre>{JSON.stringify(edges, null, 2)}</pre>
