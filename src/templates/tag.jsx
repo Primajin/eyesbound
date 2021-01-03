3c49de425248de86445b60967e082492a57eebec
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-03 16:28:57 +0100

    show images according to tag

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const Tag = ({ data: { prismicTags } }) => {
-  const { data } = prismicTags;
+import Picture from '../components/picture';
+
+const Tag = ({ data: { prismicTags, allPrismicPicture } }) => {
+  const { data: tagData } = prismicTags;
+  const { edges: pictureData } = allPrismicPicture;
   return (
     <>
-      <h1>Tag: {data.title}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Tag: {tagData.title}</h1>
+      <pre>{JSON.stringify(tagData, null, 2)}</pre>
+      {pictureData.map(({ node: { data, id, uid } }) => (
+        <a key={id} href={`/picture/${uid}`} aria-label="link-to-picture">
+          <Picture data={data} />
+        </a>
+      ))}
     </>
   );
 };
@@ -19,6 +27,20 @@ export const pageQuery = graphql`
       uid
       data {
         title
+      }
+    }
+    allPrismicPicture(filter: { data: { tags: { elemMatch: { tag: { uid: { eq: $uid } } } } } }) {
+      edges {
+        node {
+          data {
+            title
+            image {
+              url
+            }
+          }
+          uid
+          id
+        }
       }
     }
   }
