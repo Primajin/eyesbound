9e8565da69208ef97700174b803cb70b3cbaa1e5
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-16 13:44:05 +0100

    add reset css

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {css} from '@emotion/react';
 
 import Query from '../types/proptypes.js';
+
+const imageTemporary = css`
+	background-color: grey;
+`;
 
 const Picture = ({data}) => {
 	return (
 		<>
 			<h1>{data && data.title}</h1>
-			{data && data.image && <img src={data.image.url} alt={data.image.alt} width="300"/>}
+			{data && data.image && <img css={imageTemporary} src={data.image.url} alt={data.image.alt} width="300"/>}
 			<pre>{JSON.stringify(data, null, 2)}</pre>
 		</>
 	);
