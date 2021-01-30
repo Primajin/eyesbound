2ee43541334917719c74efddf724fdd55d6f970d
Jannis Hell <Primajin@users.noreply.github.com>
2021-01-30 17:05:06 +0100

    make slideshow dynamic

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {graphql} from 'gatsby';
 
-import Picture from '../components/picture.jsx';
 import Query from '../types/proptypes.js';
 import Slideshow from '../components/slideshow.jsx';
 
@@ -14,13 +13,6 @@ const Home = ({
 	return (
 		<>
 			<h1>Home ({edges.length})</h1>
-			<p>Get only top 5 homepage=true pictures:</p>
-			{edges.map(({node: {data, id, uid}}) => (
-				<a key={id} href={`/picture/${uid}`} aria-label="link-to-picture">
-					<Picture data={data}/>
-				</a>
-			))}
-			<pre>{JSON.stringify(edges, null, 2)}</pre>
 			<Slideshow images={edges}/>
 		</>
 	);
