32a7aca02fc724b74d2c418252871738d3db4dd3
Jannis Hell <Primajin@users.noreply.github.com>
2021-02-01 15:43:43 +0100

    :cinema: enable fullscreen

---
+++ src/utils/fullscreen.js
@@ -0,0 +1,20 @@
+/* global document */
+export const fullscreenElement = document.fullscreenElement || document.mozFullscreenElement || document.msFullscreenElement || document.webkitFullscreenDocument;
+
+/**
+ * Toggles the specified Element into fullscreen mode - otherwise whole document
+ * @param {string} [selector] the css selector; falls back to documentElement if undefined or not found
+ */
+export const toggleFullscreen = selector => {
+	document.fullscreenElement = fullscreenElement;
+	document.exitFullscreen = document.exitFullscreen || document.mozExitFullscreen || document.msExitFullscreen || document.webkitExitFullscreen;
+	const queriedElement = selector && selector.length > 0 && document.querySelector(selector);
+	const element = queriedElement || document.documentElement;
+	element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.msRequestFullscreen || element.webkitRequestFullscreen;
+
+	if (!document.fullscreenElement) {
+		element.requestFullscreen();
+	} else if (document.exitFullscreen) {
+		document.exitFullscreen();
+	}
+};
