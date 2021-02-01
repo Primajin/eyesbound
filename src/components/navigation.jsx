20ff7b8ba246388a37aa1f37d347c1fef00490e0
Jannis Hell <Primajin@users.noreply.github.com>
2021-02-01 13:50:49 +0100

    add transition to menu with dynamic length

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -4,18 +4,44 @@ import React from 'react';
 const nav = css`
 	background-color: var(--background);
 	color: var(--foreground);
-	height: 40px;
 	line-height: 40px;
+	outline: 0;
 	position: fixed;
 	text-align: center;
 	top: 100px;
+	transform: translateY(calc(-100% + 40px));
+	transition: transform .3s;
+	user-select: none;
 	width: 180px;
 	z-index: 9;
+
+	&:focus {
+		transform: translateY(0);
+	}
+
+	ul {
+		background-color: var(--foreground);
+		color: var(--background);
+	}
+
+	li {
+		transition: opacity .3s;
+
+		&:hover {
+			opacity: 0.666;
+		}
+	}
+
+	div, li {
+		cursor: pointer;
+		font-weight: 700;
+		text-transform: uppercase;
+	}
 `;
 
 const Navigation = () => {
 	return (
-		<nav css={nav}>
+		<nav css={nav} tabIndex="1">
 			<ul>
 				<li>Hello</li>
 				<li>World</li>
@@ -27,6 +53,7 @@ const Navigation = () => {
 				<li>Fine</li>
 				<li>Thanks</li>
 			</ul>
+			<div>Menu</div>
 		</nav>
 	);
 };
