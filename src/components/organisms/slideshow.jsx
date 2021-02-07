27896103b19ce68bff412063fd3a804eadc9198a
Jannis Hell <Primajin@users.noreply.github.com>
2021-02-07 12:10:15 +0100

    :lipstick: style the interaction on the homepage

--- src/components/organisms/slideshow.jsx
+++ src/components/organisms/slideshow.jsx
@@ -6,6 +6,7 @@ import {css} from '@emotion/react';
 import '../../styles/slideshow.css';
 
 import {prismicPictureNode} from '../../types/proptypes.js';
+import {up} from '../../utils/theming.js';
 
 const wrapper = css`
 	height: calc(100vh - 52px);
@@ -32,7 +33,7 @@ const indicator = css`
 const image = css`
 	border: 2px solid transparent;
 	cursor: pointer;
-	transition: border-color .33s;
+	transition: border-color .333s;
 	margin-bottom: -2px;
 `;
 
@@ -45,10 +46,38 @@ const indicatorFigCaption = css`
 	right: 20px;
 	text-align: right;
 	text-transform: uppercase;
-	transition: opacity .33s;
+	transition: opacity .333s;
 
 	.active & {
 		opacity: 1;
+	}
+`;
+
+const arrowButtons = css`
+	background: var(--foreground);
+	color: var(--background);
+	height: 180px;
+	position: fixed;
+	text-align: center;
+	top: 50%;
+	transform: translateY(-50%);
+	transition: background-color .333s, color .333s;
+	width: 40px;
+	z-index: 5;
+
+	${up('md')} {
+		width: 20px;
+	};
+
+	&:hover,
+	&:focus {
+		background: var(--background);
+		color: var(--foreground);
+		outline: 0;
+	}
+
+	&[data-type="next"] {
+		right: 0;
 	}
 `;
 
@@ -66,17 +95,20 @@ const Slideshow = ({images}) => {
 			return (
 				<li className="indicator" css={indicator} title={title}>
 					<figure>
-						<img css={image} src={url} alt={alt || title}/>
+						<img alt={alt || title} css={image} src={url}/>
 						<figcaption css={indicatorFigCaption}>{title}</figcaption>
 					</figure>
 				</li>
 			);
-		}
+		},
+		nextArrow: <button css={arrowButtons} tabIndex="4" type="button">»</button>,
+		pauseOnHover: false,
+		prevArrow: <button css={arrowButtons} tabIndex="3" type="button">«</button>
 	};
 
 	return (
 		<div className="slide-container">
-			<Fade {...properties} pauseOnHover={false}>
+			<Fade {...properties}>
 				{images.map(({node: {data: {title, image: {alt, url}}, id, uid}}) => (
 					<a key={id} title={title} css={wrapper} href={`/picture/${uid}`}>
 						<figure css={figure} style={{backgroundImage: `url(${url})`}}>
--- src/utils/theming.js
+++ src/utils/theming.js
@@ -1,6 +1,13 @@
-/* global window */
 /**
  * Does user prefer dark mode UI in Operation System settings?
  * @type {boolean}
  */
 export const userPrefersDark = window && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const breakpoints = new Map([['sm', 576], ['md', 768], ['lg', 992], ['xl', 1200]]);
+/**
+ * Gets media query for given breakpoint
+ * @param {('sm'|'md'|'lg'|'xl')} breakpoint The name of the breakpoint
+ * @returns {('576px'|'768px'|'992px'|'1200px')} The media query with min-width going UP
+ */
+export const up = breakpoint => `@media (min-width: ${breakpoints.get(breakpoint)}px)`;
