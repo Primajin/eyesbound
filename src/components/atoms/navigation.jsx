2a996d72829d272089f0cccd119084056b0ea090
Jannis Hell <Primajin@users.noreply.github.com>
2021-02-07 14:33:26 +0100

    :open_book: style the menu

--- src/components/atoms/navigation.jsx
+++ src/components/atoms/navigation.jsx
@@ -1,5 +1,8 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import classnames from 'classnames';
 import {css} from '@emotion/react';
+
+import {up} from '../../utils/theming.js';
 
 const nav = css`
 	line-height: 40px;
@@ -13,10 +16,18 @@ const nav = css`
 	width: 180px;
 	z-index: 9;
 
+	${up('md')} {
+		transform: translateY(calc(-100% + 20px));
+	};
+
 	div {
 		transition: background-color .333s, color .333s;
 		background-color: var(--background);
 		color: var(--foreground);
+
+		${up('md')} {
+			line-height: 20px;
+		};
 
 		&:hover {
 			background: var(--foreground);
@@ -24,7 +35,7 @@ const nav = css`
 		}
 	}
 
-	&:focus {
+	&.open {
 		transform: translateY(0);
 
 		div {
@@ -59,27 +70,41 @@ const nav = css`
 
 const Navigation = () => {
 	const navigationElement = useRef(null);
+	const [menuOpen, setMenuOpen] = useState(false);
 
-	const blur = () => {
+	const openMenu = () => {
 		if (document.activeElement === navigationElement.current) {
-			navigationElement.current.blur();
+			setMenuOpen(true);
 		}
 	};
 
+	useEffect(() => {
+		// Register eventListener once
+		document.addEventListener('focus', openMenu, true);
+
+		return () => {
+			// Unregister eventListener once
+			document.removeEventListener('focus', openMenu, true);
+		};
+	}, []);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
 	return (
-		<nav ref={navigationElement} css={nav} tabIndex="2">
+		<nav ref={navigationElement} css={nav} className={classnames({open: menuOpen})} tabIndex="2">
 			<ul>
-				<li><a href="/">Hello</a></li>
-				<li><a href="/">World</a></li>
-				<li><a href="/">How</a></li>
-				<li><a href="/">Are</a></li>
-				<li><a href="/">You</a></li>
-				<li><a href="/">I</a></li>
-				<li><a href="/">am</a></li>
-				<li><a href="/">Fine</a></li>
-				<li><a href="/">Thanks</a></li>
+				<li><a href="/picture" tabIndex="3">Overview</a></li>
+				<li><a href="/series/shuttered" tabIndex="4">»Shuttered«</a></li>
+				<li><a href="/category/architecture" tabIndex="5">Architecture</a></li>
+				<li><a href="/category/environment" tabIndex="6">Environment</a></li>
+				<li><a href="/category/floral" tabIndex="7">Floral</a></li>
+				<li><a href="/category/light" tabIndex="8">Light</a></li>
+				<li><a href="/worldmap" tabIndex="9">Worldmap</a></li>
+				<li><a href="/imprint" tabIndex="10">Contact</a></li>
 			</ul>
-			<div onClick={blur}>Menu</div>
+			<div onClick={toggleMenu}>Menu</div>
 		</nav>
 	);
 };
