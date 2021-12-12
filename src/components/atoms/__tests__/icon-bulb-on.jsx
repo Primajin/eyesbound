0f02b0d97f15ec9772ea11c85027b3dfdf753ec7
Jannis Hell <Primajin@users.noreply.github.com>
2021-12-12 12:42:45 +0100

    :white_check_mark: add tests for icons

---
+++ src/components/atoms/__tests__/icon-bulb-on.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {create} from 'react-test-renderer';
+
+import BulbOn from '../icons/bulb-on.jsx';
+
+describe('BulbOn', () => {
+	it('renders correctly', () => {
+		const component = create(<BulbOn/>).toJSON();
+		expect(component).toMatchSnapshot();
+	});
+});
---
+++ src/components/atoms/__tests__/icon-fullscreen-exit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {create} from 'react-test-renderer';
+
+import FullscreenExit from '../icons/fullscreen-exit.jsx';
+
+describe('FullscreenExit', () => {
+	it('renders correctly', () => {
+		const component = create(<FullscreenExit/>).toJSON();
+		expect(component).toMatchSnapshot();
+	});
+});
