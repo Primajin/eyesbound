84ca8b8e4e1b54d30b762e8f7fe1ff162735c108
Jannis Hell <Primajin@users.noreply.github.com>
2021-12-28 10:38:13 +0100

    :computer_mouse: add tests for clicks

--- src/components/atoms/__tests__/theme-switcher.test.jsx
+++ src/components/atoms/__tests__/theme-switcher.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {create} from 'react-test-renderer';
+import {act, create} from 'react-test-renderer';
 
 import ThemeSwitcher from '../theme-switcher.jsx';
 
@@ -12,5 +12,13 @@ describe('ThemeSwitcher', () => {
 	it('renders correctly with props', () => {
 		const component = create(<ThemeSwitcher isFullscreen/>).toJSON();
 		expect(component).toMatchSnapshot();
+	});
+
+	it('renders correctly when theme is toggled', () => {
+		const component = create(<ThemeSwitcher/>);
+		act(() => {
+			component.root.findByType('button').props.onClick();
+		});
+		expect(component.toJSON()).toMatchSnapshot();
 	});
 });
